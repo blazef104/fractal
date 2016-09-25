69f957c8d58ff329e50b0e665469e3729b753815
Blaze <nanc23@rocketmail.com>
2016-09-25 19:17:35 +0200

    improvements, see changelog in readme

--- num.py
+++ num.py
@@ -3,52 +3,76 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from PIL import Image
 
+width = 900
+height = 900
 class Win(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title = "Fractal Generator")
         vbox = Gtk.Box(spacing = 5, orientation = Gtk.Orientation.VERTICAL)
         hbox = Gtk.Box(spacing = 5)
-
+        hbox1 = Gtk.Box(spacing = 5)
         self.add(vbox)
 
         label = Gtk.Label("Generate your fractal, more features to come...")
         vbox.pack_start(label, True, True, 0)
 
+
+
         self.pbar = Gtk.ProgressBar()
         self.pbar.set_show_text(True)
         vbox.pack_start(self.pbar, True, True, 0)
 
+        labelW = Gtk.Label("Width: ")
+        self.entryWidth = Gtk.Entry()
+        self.entryWidth.set_text(str(width))
+
+        labelH = Gtk.Label("Height: ")
+        self.entryHeight = Gtk.Entry()
+        self.entryHeight.set_text(str(height))
+
+        hbox1.pack_start(labelW, True, True, 0)
+        hbox1.pack_start(self.entryWidth, True, True, 0)
+        hbox1.pack_start(labelH, True, True, 0)
+        hbox1.pack_start(self.entryHeight, True, True, 0)
+        vbox.pack_start(hbox1, True, True, 0)
+
         vbox.pack_start(hbox, True, True, 0)
 
-        button = Gtk.Button.new_with_label("Generate SVG")
-        button.connect("clicked", self.fract_svg)
+        radioBox = Gtk.Box(spacing=5)
+        self.svgButton = Gtk.CheckButton.new_with_label("Generate as .SVG file")
+        self.pngButton = Gtk.CheckButton.new_with_label("Generate as .PNG file")
+        self.pngButton.set_active(True)
+        self.svgButton.set_active(False)
+        radioBox.pack_start(self.svgButton, True, True, 0)
+        radioBox.pack_start(self.pngButton, True, True, 0)
+        vbox.pack_start(radioBox, True, True, 0)
 
-        button1 = Gtk.Button.new_with_label("Generate PNG")
-        button1.connect("clicked", self.fract_png)
+
+        button = Gtk.Button.new_with_label("Generate!")
+        button.connect("clicked", self.fract)
 
         hbox.pack_start(button, True, True, 0)
-        hbox.pack_start(button1, True, True, 0)
 
-    def fract_svg(self, button):
+    def fract(self, button):
         self.pbar.set_text("Generating SVG file")
-        generateMandelbrot()
-        return
-
-    def fract_png(self, button1):
-        self.pbar.set_text("Generating PNG file")
-        # self.pbar.set_fraction(perc)
-        # self.pbar.pulse()
+        global width, height
+        width = int(self.entryWidth.get_text())
+        height = int(self.entryHeight.get_text())
+        if (self.svgButton.get_active()):
+            print("Generating svg. Dimension: ", width, height)
+            generateMandelbrot()
+            return
+        print("Generating png. Dimension: ", width, height)
         generateMandelbrot_new()
         return
 
 def generateMandelbrot_new():
-    print("generating png")
-    img = Image.new("RGB", (600, 600))
+    img = Image.new("RGB", (width, height))
     arr = []
     a = 0
-    while(a<600):
+    while(a<width):
         b = 0
-        while (b<600):
+        while (b<height):
             checkMembership(a, b, arr)
             b+=1
         a+=1
@@ -58,11 +82,11 @@ def generateMandelbrot_new():
     return
 
 def generateMandelbrot():
-    draw = svgwrite.Drawing(filename = "test-fract.svg", size = ("600px", "600px"))
+    draw = svgwrite.Drawing(filename = "test-fract.svg", size = (str(width)+"px", str(width)+"px"))
     a = 0
-    while (a<=600):
+    while (a<=width):
         b = 0
-        while (b<=600):
+        while (b<=height):
             checkMembership(a,b, draw)
             b+=1
         a+=1
@@ -73,31 +97,28 @@ def generateMandelbrot():
 
 
 def checkMembership(x, y, draw):
-    #print("checking", x, y)
-    x1 = (x-450)/200
-    y1 = (y-300)/200
+    x1 = (x-(width*0.7))/(width*0.28)
+    y1 = (y-(height-(height/2)))/(height/2)
 
 
-    print("checking", x1, y1)
+    #print("checking", x1, y1)
     z0 = 0
     z = complex(x1, y1)
     n = 0
     while(n<256):
         z0 = z0**2 + z
         if(abs(z0)>200):
-            #drawPoint(x, y, draw, color="white")
             if(type(draw) is list):
-                print("diverges")
                 draw.append((0,0,255))
                 return
-            print("diverges with z=", abs(z0))
+            #print("diverges with z=", abs(z0))
             return
         n+=1
     if(type(draw) is list):
         draw.append((0,255,0))
         return
     drawPoint(x,y, draw, color="black")
-    print("converges")
+    #print("converges")
     return
 
 def drawPoint(x, y, draw, color):
--- readme.md
+++ readme.md
@@ -1,6 +1,18 @@
 Fractal Generator
 =================
-A very simple and inefficent mandelbrot set generator for learning purpose, i plan to improve it in the future(maybe).
+A very simple and inefficent mandelbrot set generator for learning purpose.
+I still have to figure out if my generation method is efficent, i think the png image is good but still needs some adjustments to be more customizable by the user.
 
->[Image](/test-fract.svg)
+>[Svg file](/test-fract.svg) \
+>[Png file](/fract.png)
 
+###Changelog
+* Added some control over the image generation
+* Small changes to the interface
+
+###Todos
+- [] Make the interface more userfrendly
+- [] Users can modify equations
+- [] Add "zooming"
+- [] Add Julia set
+- [x] User can control image dimension
